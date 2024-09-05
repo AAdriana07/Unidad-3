5ab695f2d64eb0275d81f1602453184ae55b7eee
AAnet07 <alexiavillnetp.2020@gmail.com>
2024-09-04 19:46:08 -0500

    objectos-familia

---
+++ Semana 10/repaso-objectos/assets/js/script.js
@@ -0,0 +1,57 @@
+const familia = {
+    padres: [
+        {
+            nombre: 'Josefa',
+            apellido: 'Pongo',
+            edad: 45,
+            hobbie: 'bailar',
+        },
+        {
+            nombre: 'Luis',
+            apellido: 'Villenet',
+            edad: 55,
+            hobbie: 'cocinar',
+        },
+    ],
+
+    abuelos: {
+        maternos: [
+            {
+                nombre: 'Bacilia',
+                apellido: 'Facundo',
+                edad: 65,
+                hobbie: 'cocinar',
+            },
+            {
+                nombre: 'Josue',
+                apellido: 'Facundo',
+                edad: 67,
+                hobbie: 'jugar futbol',
+            },
+        ],
+        paternos: [
+            {
+                nombre: 'Rosa',
+                apellido: 'Villanueva',
+                edad: 60,
+                hobbie: 'cocinar',
+            },
+            {
+                nombre: 'Juan',
+                apellido: 'Vazques',
+                edad: 77,
+                hobbie: 'ver películas',
+            },
+        ]
+    }
+};
+
+console.log(familia);
+//Aceder 
+console.log(familia.padres[0].nombre);
+console.log(familia.padres[0]["apellido"]);
+console.log(familia.padres[1].hobbie);
+console.log(familia.padres[0]["nombre"]);
+//Modificar 
+familia.padres[1].nombre = "Carlos";
+familia.abuelos.maternos[1].hobbie = "Bailar";
